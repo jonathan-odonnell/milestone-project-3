fc69676013d15c19a90390fdac7d5f1393229f71
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2020-10-30 22:16:46 +0000

    Linked sign up form to database

--- run.py
+++ run.py
@@ -53,8 +53,28 @@ def sign_in():
     return render_template("sign_in.html", page_title="Sign In")
 
 
-@app.route("/sign_up")
+@app.route("/sign_up", methods=["GET", "POST"])
 def sign_up():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"email": request.form.get("email").lower()})
+
+        if existing_user:
+            flash("Email already registered")
+            return redirect(url_for("sign_up"))
+
+        sign_up = {
+            "first_name": request.form.get("first_name").lower(),
+            "last_name": request.form.get("last_name").lower(),
+            "email": request.form.get("email").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(sign_up)
+
+        session["user"] = request.form.get("first_name").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("profile", first_name=session["user"]))
+
     return render_template("sign_up.html", page_title="Sign Up")
 
 
