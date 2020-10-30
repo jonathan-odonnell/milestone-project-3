45983531de6e5dd1d7895818b238eba3c609ed0c
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2020-10-30 21:05:48 +0000

    Added profile page

--- run.py
+++ run.py
@@ -1,7 +1,20 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, flash, render_template, redirect, request, session, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+mongo = PyMongo(app)
 
 
 @app.route("/")
@@ -43,6 +56,16 @@ def sign_in():
 @app.route("/sign_up")
 def sign_up():
     return render_template("sign_up.html", page_title="Sign Up")
+
+
+@app.route("/profile/<first_name>", methods=["GET", "POST"])
+def profile(first_name):
+    first_name = session["user"]
+
+    if session["user"]:
+        return render_template("profile.html", first_name=first_name)
+
+    return redirect(url_for("sign_in"))
 
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
