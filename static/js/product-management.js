aeb12500d449ba7ef2f353865dc6f866bf7ec33d
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2020-12-06 23:30:17 +0000

    Fixed validation and added missing ID's in add product and edit product forms

--- static/js/product-management.js
+++ static/js/product-management.js
@@ -1,6 +1,11 @@
 $('#product-details').find('.btn').on('click', function () {
-    $('#product-details').addClass('d-none')
-    $('#product-features').removeClass('d-none')
+    // Code is from https://stackoverflow.com/questions/7386817/html5-form-checkvalidity-method-not-found and https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/reportValidity
+    if ($('form')[1].reportValidity() == false) {
+        return
+    } else {
+        $('#product-details').addClass('d-none')
+        $('#product-features').removeClass('d-none')
+    }
 })
 
 $('#product-features').find('.btn').first().on('click', function () {
