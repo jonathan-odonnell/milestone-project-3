6b45d54499c374feef0f446d7decac1da6e8f5d2
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2020-12-22 01:00:15 +0000

    Added functionality to check form is valid before submitting and added comments in contact.js

--- static/js/contact.js
+++ static/js/contact.js
@@ -1,10 +1,16 @@
-
-// Code for getting data inputted into the form is from https://stackoverflow.com/questions/25881204/how-to-use-jquery-post-method-to-submit-form-values
-
+// Checks the form is valid and posts the data entered to the /newsletter url and updates the html when the contact form send message button is clicked.
 $('#contact-form').find('.btn').on('click', function (e) {
+    // Prevents the button's default behaviour
     e.preventDefault()
-    $.post("/newsletter", $("#contact-form").serialize()).done(function () {
-        $('#contact-form').parent().prepend('<p class="text-center my-5">Thank you for your message. A member of the team will be in touch shortly</p>')
-        $('#contact-form').remove()
-    });
+    // Checks if the form is valid and if it is not reports its validity. Code is from https://developer.mozilla.org/en-US/docs/Learn/Forms/Form_validation and https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/reportValidity
+    if ($('#contact-form')[0].checkValidity() === false) {
+        $('#contact-form')[0].reportValidity()
+    } else {
+        // checks that the form is valid and posts the data inputted to the /newsletter url. Code is from https://stackoverflow.com/questions/25881204/how-to-use-jquery-post-method-to-submit-form-values and https://stackoverflow.com/questions/7426085/jquery-getting-form-values-for-ajax-post
+        $.post("/newsletter", $("#contact-form").serialize()).done(function () {
+            //If a success status is returned, the HTML below is added to the DOM as the contact form's previous sibling and the contact form is removed from the DOM.
+            $('#contact-form').parent().prepend('<p class="text-center">Thank you for your message. A member of the team will be in touch shortly</p>')
+            $('#contact-form').remove()
+        });
+    }
 });
