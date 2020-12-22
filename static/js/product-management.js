09832793a8a769e7747614dbcb2263e992e16d35
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2020-12-22 17:34:25 +0000

    Refactored code and added comments to product-management.js

--- static/js/product-management.js
+++ static/js/product-management.js
@@ -1,7 +1,13 @@
-$(document).ready(function() {
+/* Sets the sort by dropdown button as the value of the sort search parameters
+if the value is not null. Current url code is from https://
+developer.mozilla.org/en-US/docs/Web/API/Window/location and search query sort
+parameters code is from https://developer.mozilla.org/en-US/docs/Web/API/URL/
+searchParams */
+
+$(document).ready(function () {
     let currentUrl = new URL(window.location);
     let search = currentUrl.searchParams.get("sort")
-    if (search !== undefined) {
+    if (search !== null) {
         if (search === "a-to-z") {
             $('#product-filter').html("Name A - Z")
         } else if (search === "z-to-a") {
@@ -14,8 +20,28 @@ $(document).ready(function() {
     }
 })
 
+/* Sets the sort search parameter when a link in the sort by dropdown is
+clicked and reloads the page. Current url code is from https://
+developer.mozilla.org/en-US/docs/Web/API/Window/location. Search query sort
+parameters code is from https://developer.mozilla.org/en-US/docs/Web/API/URL/
+searchParams url replace code is from https://developer.mozilla.org/en-US/
+docs/Web/API/Location/replace */
+
+$('#product-filter').next().children().on('click', function () {
+    let currentUrl = new URL(window.location);
+    let sort = $(this).val();
+    currentUrl.searchParams.set("sort", sort);
+    window.location.replace(currentUrl);
+})
+
+/* When the next button is clicked on the add product or edit product page,
+checks the form is valid and hides the div containing the product-details ID
+and shows the div containing the product-features ID. Code for checking the
+form validity is from https://stackoverflow.com/questions/7386817/
+html5-form-checkvalidity-method-not-found and https://developer.mozilla.org/
+en-US/docs/Web/API/HTMLFormElement/reportValidity */
+
 $('#product-details').find('.btn').on('click', function () {
-    // Code is from https://stackoverflow.com/questions/7386817/html5-form-checkvalidity-method-not-found and https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/reportValidity
     if ($('form')[2].reportValidity() == false) {
         return
     } else {
@@ -24,15 +50,11 @@ $('#product-details').find('.btn').on('click', function () {
     }
 })
 
+/* When the back button is clicked on the add product or edit product page,
+hides the div containing the product-features ID and shows the div containing
+the product-details ID. */
+
 $('#product-features').find('.btn').first().on('click', function () {
     $('#product-details').removeClass('d-none')
     $('#product-features').addClass('d-none')
-})
-
-$('#product-filter').next().children().on('click', function() {
-    let currentUrl = new URL(window.location);
-    let sort = $(this).val();
-    html = $(this).html()
-    currentUrl.searchParams.set("sort", sort);
-    window.location.replace(currentUrl);
 })
