a4f5e4d6ee8269b7ff8f538b9ac422bcb341f369
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2020-12-22 17:06:52 +0000

    Renamed scripts.js to base.js and refactored code and added comments in base.js

---
+++ static/js/base.js
@@ -0,0 +1,20 @@
+// Adds minus icon to the accordion item headers when the item's body is shown. Collapse show code is from https://getbootstrap.com/docs/4.5/components/collapse/#methods. CSS classes for the plus and minus icons are from https://fontawesome.com/icons/plus?style=solid and https://fontawesome.com/icons/minus?style=solid
+$(".collapse").on("show.bs.collapse", function () {
+    $(this)
+        .prev(".card-header")
+        .find(".fas")
+        .removeClass("fa-plus")
+        .addClass("fa-minus");
+})
+
+// Adds minus icon to the accordion item headers when the item's body is hidden. Collapse hide code is from https://getbootstrap.com/docs/4.5/components/collapse/#methods. CSS classes for the plus and minus icons are from https://fontawesome.com/icons/plus?style=solid and https://fontawesome.com/icons/minus?style=solid
+$(".collapse").on("hide.bs.collapse", function () {
+    $(this)
+        .prev(".card-header")
+        .find(".fas")
+        .removeClass("fa-minus")
+        .addClass("fa-plus");
+});
+
+// Shows the toasts. Code is from https://getbootstrap.com/docs/4.5/components/toasts/
+$('.toast').toast('show');
