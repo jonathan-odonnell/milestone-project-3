0e5fe3d8e2443abd244e2d4f5985fe1945aeee0d
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2020-12-24 20:51:36 +0000

    Amended comments in contact.js, index.js and sign-up.html

--- static/js/index.js
+++ static/js/index.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
     });
 });
 
-// Checks the email address entered in the newsletter sign up form is valid, posts it to the /newsletter url and dynamically updates the HTML with a response once a success status has been returned. Code is from https://stackoverflow.com/questions/7386817/html5-form-checkvalidity-method-not-found, https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/reportValidity,https://stackoverflow.com/questions/7426085/jquery-getting-form-values-for-ajax-post and https://stackoverflow.com/questions/9824808/disable-form-auto-submit-on-button-click/9825224
+// Checks the email address entered in the newsletter sign up form is valid, posts it to the /newsletter url and dynamically updates the HTML with a response once a success status has been returned. Code for preventing the default behaviour of the submit button is from https://stackoverflow.com/questions/9824808/disable-form-auto-submit-on-button-click/9825224. Code for checking the form validity is from https://stackoverflow.com/questions/7386817/html5-form-checkvalidity-method-not-found and https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/reportValidity. Code for posting the form data and updating the HTML is from https://stackoverflow.com/questions/7426085/jquery-getting-form-values-for-ajax-post.
 
 $('#newsletter-sign-up .btn').on('click', function (e) {
     e.preventDefault()
